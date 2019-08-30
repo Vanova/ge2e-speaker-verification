c04530ca6c538cfb4a619ba8d6b0d06fd7546636
Vanova <iva427@mail.ru>
2019-08-30 14:59:40 +0800

    fix train.sh, add conda env

--- train.sh
+++ train.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#set -eu
 
-set -eu
+export PYTHONPATH="$(pwd)/ge2e/:$PYTHONPATH"
+source activate ai
 
 train_steps=2500
 dev_steps=800
@@ -10,19 +12,21 @@ epochs=50
 
 echo "$0 $@"
 
-[ $# -ne 1 ] && echo "Script format error: $0 <gpuid> <cpt-id>" && exit 1
+[ $# -ne 1 ] && echo "Script format error: $0 <gpuid>" && exit 1
+
+mkdir -p $cpt_dir
 
 # DEBUG
-#./ge2e/train_ge2e.py --M 2 --N 2 --gpu 0 --epochs 1 --train-steps 100 --dev-steps 80 --chunk-size "140,180" --checkpoint ../exp/ge2e/1
+#./ge2e/train_ge2e.py --M 2 --N 2 --gpu 0 --epochs 1 --train-steps 100 --dev-steps 80 --chunk-size "140,180" --checkpoint $cpt_dir
 
 # by Kaldi NISR_SRE_2018 M=8
- ./ge2e/train_ge2e.py \
-   --M 8 \
-   --N 64 \
-   --gpu $1 \
-   --epochs $epochs \
-   --train-steps $train_steps \
-   --dev-steps $dev_steps \
-   --chunk-size $chunk_size \
-   --checkpoint $cpt_dir \
-   > $cpt_dir/train.log 2>&1
+./ge2e/train_ge2e.py \
+  --M 8 \
+  --N 64 \
+  --gpu $1 \
+  --epochs $epochs \
+  --train-steps $train_steps \
+  --dev-steps $dev_steps \
+  --chunk-size $chunk_size \
+  --checkpoint $cpt_dir \
+  > $cpt_dir/train.log 2>&1
